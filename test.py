76b9d6972a65259eae215ab7b608cd0709169e9c
Richard Feynman <rhcfeynman@gmail.com>
2021-06-07 10:10:10 +0700

    First Commit

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ def make_commit(days: int):
         return os.system('git push')
     else:
         #dates = f'{days} days ago'
-        dates = "2021-6-6 10:10:10"
+        dates = "2021-6-7 10:10:10"
 
         with open('data.txt', 'a') as file:
             file.write(f'{dates}\n')
